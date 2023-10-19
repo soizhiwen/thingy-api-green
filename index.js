ade0e4e2c0eba1db44af16cc4d0bb89908af76d3
Lukas Odermatt <83284905+GrischuG@users.noreply.github.com>
2023-10-19 20:53:14 +0200

    Lukas (#2)
    * Added simple MQTT capabilities
    
    * More API setup
    - Added some simple '/plants/' routes with placeholder functions (get, post, patch, del)
    - Added placeholder function for adding Data via MQTT
    
    * Update .gitignore

--- index.js
+++ index.js
@@ -7,11 +7,55 @@ const cors = require('@koa/cors');
 const app = new Koa();
 
 
+router.get('/plants/', listPlants)
+    .post('/plants/', addPlant)
+    .patch('/plants/:id', updatePlant)
+    .del('/plants/:id', removePlant)
+
+
+
+async function listPlants(ctx) {
+    console.log("GET request to list all plants received!");
+    // Return all plants
+}
+
+async function addPlant(ctx) {
+    console.log("POST request to add a plants received!");
+    const data = ctx.request.body;
+    // Call DB Function
+}
+
+async function updatePlant(ctx) {
+    console.log("PATCH request to change a plant's value received!");
+    // Update in DB
+}
+
+async function removePlant(ctx) {
+    console.log("DELETE request received!");
+    // Remove plant from DB
+}
+
+async function addData(topic, message) {
+    // Split Json Package and add in DB accordingly
+}
+
+async function deleteMeasurements() {
+    // Irreversibly removes all measured data from the DB
+}
+
+
+
+
+
+
+/*
 app.use(ctx => {
 
     ctx.body = "Hello world";
     
 });
+
+ */
 
 
 app
