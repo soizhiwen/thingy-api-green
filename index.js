5c4eb59dc09930bb567ecc4cc0580d32a71f2c8b
soizhiwen <zhiwen39@gmail.com>
2023-10-22 15:52:36 +0200

    Connect to influxdb

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
-const router = require('koa-router')();
+const { writeClient, queryClient } = require("./models");
+const router = require("koa-router")();
 
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
 
 const app = new Koa();
 
-
-router.get('/plants/', listPlants)
-    .post('/plants/', addPlant)
-    .patch('/plants/:id', updatePlant)
-    .del('/plants/:id', removePlant)
-
-
+router
+  .get("/plants/", listPlants)
+  .post("/plants/", addPlant)
+  .patch("/plants/:id", updatePlant)
+  .del("/plants/:id", removePlant);
 
 async function listPlants(ctx) {
-    console.log("GET request to list all plants received!");
-    // Return all plants
+  console.log("GET request to list all plants received!");
+  // Return all plants
 }
 
 async function addPlant(ctx) {
-    console.log("POST request to add a plants received!");
-    const data = ctx.request.body;
-    // Call DB Function
+  console.log("POST request to add a plants received!");
+  const data = ctx.request.body;
+  // Call DB Function
 }
 
 async function updatePlant(ctx) {
-    console.log("PATCH request to change a plant's value received!");
-    // Update in DB
+  console.log("PATCH request to change a plant's value received!");
+  // Update in DB
 }
 
 async function removePlant(ctx) {
-    console.log("DELETE request received!");
-    // Remove plant from DB
+  console.log("DELETE request received!");
+  // Remove plant from DB
 }
 
 async function addData(topic, message) {
-    // Split Json Package and add in DB accordingly
+  // Split Json Package and add in DB accordingly
 }
 
 async function deleteMeasurements() {
-    // Irreversibly removes all measured data from the DB
+  // Irreversibly removes all measured data from the DB
 }
-
-
-
-
-
 
 /*
 app.use(ctx => {
@@ -57,13 +51,12 @@ app.use(ctx => {
 
  */
 
-
 app
   .use(bodyParser())
   .use(cors())
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(8080,()=>{
-    console.log(`Application running on port 8080`);
+app.listen(8080, () => {
+  console.log(`Application running on port 8080`);
 });
