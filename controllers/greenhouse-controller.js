a51a04558fa61c6b0c0cfb2a535c1f90924ab684
soizhiwen <zhiwen39@gmail.com>
2023-10-31 10:58:53 +0100

    Add time series data to database

--- controllers/greenhouse-controller.js
+++ controllers/greenhouse-controller.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
+const { Point } = require("@influxdata/influxdb-client");
+const { writeClient, queryClient } = require("../models");
+
+const thingyMonitor = process.env.THINGY_MONITOR;
+
 async function listPlants(ctx) {
   console.log("GET request to list all plants received!");
   // Return all plants
@@ -16,13 +22,23 @@ async function getData(ctx) {
 }
 
 async function addData(message) {
-  console.log("Adding data from MQTT transmission.");
-  console.log("MQTT message to add: " + message);
-
-  // Preliminary implementation, expansion needed ->
-  // Split Json Package and add in DB accordingly
-
-  // AddToDB !!
+  const messageJson = JSON.parse(message.toString());
+  writeClient.useDefaultTags({ thingy_id: thingyMonitor });
+  const point = new Point("mqtt_message").tag("app_id", messageJson.appId);
+  let fields;
+  if (messageJson.appId === "LIGHT") {
+    const light = messageJson.data.split(" ");
+    fields = point
+      .intField("a", light[0])
+      .intField("b", light[1])
+      .intField("c", light[2])
+      .intField("d", light[3]);
+  } else {
+    const data = parseFloat(messageJson.data);
+    fields = point.floatField("data", data);
+  }
+  writeClient.writePoint(fields);
+  console.log(` ${fields}`);
 }
 
 module.exports = { listPlants, getData, addData };
