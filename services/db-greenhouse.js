bdf5d61d986a565816a4461fe8660b6bf215871e
GrischuG <GrischuG@protonmail.com>
2023-11-02 12:54:15 +0100

    Saved change to dependency
    - Renaming of index.js from models to database.js now saved

--- services/db-greenhouse.js
+++ services/db-greenhouse.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Point } = require("@influxdata/influxdb-client");
-const { writeClient, queryClient } = require("../models");
+const { writeClient, queryClient } = require("../models/database");
 
 const bucket = process.env.INFLUX_BUCKET;
 const thingyMonitor = process.env.THINGY_MONITOR;
