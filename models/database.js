a5120a5423728861c1975f3a263eb089dbd2bb91
GrischuG <GrischuG@protonmail.com>
2023-11-07 00:50:22 +0100

    Added my DB

--- models/database.js
+++ models/database.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { InfluxDB } = require("@influxdata/influxdb-client");
 
-const token = process.env.INFLUX_TOKEN;
-const url = process.env.INFLUX_URL;
-const org = process.env.INFLUX_ORG;
-const bucket = process.env.INFLUX_BUCKET;
+const token = 'Rcnl09Tai1I6Nc7-D24-_qxWcps7KOJK_fjUMZKomZhEOF9zkfyOm1DZ3kMAAx2wcry41tUMlxBNBazgMocPUQ==';
+const url = 'http://localhost:8086';
+const org = `ASE - AS23`;
+const bucket = `Test Bucket`;
 
 const client = new InfluxDB({ url, token });
 
