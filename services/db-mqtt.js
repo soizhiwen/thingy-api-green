e466dce3054d85459ca484a9a9940882af1d42d6
soizhiwen <zhiwen39@gmail.com>
2023-11-15 20:04:47 +0100

    Filter the mqtt topic

--- services/db-mqtt.js
+++ services/db-mqtt.js
@@ -2,25 +2,19 @@ const { writeClient } = require("../models/database");
 const { Point } = require("@influxdata/influxdb-client");
 
 const thingyMonitor = process.env.THINGY_MONITOR;
+const appIds = ["AIR_QUAL", "CO2_EQUIV", "TEMP", "HUMID"];
 
 async function dbAddData(message) {
-    const messageJson = JSON.parse(message.toString());
+  const messageJson = JSON.parse(message.toString());
+
+  if (appIds.includes(messageJson.appId)) {
     writeClient.useDefaultTags({ thingy_id: thingyMonitor });
     const point = new Point("mqtt_message").tag("app_id", messageJson.appId);
-    let fields;
-    if (messageJson.appId === "LIGHT") {
-        const light = messageJson.data.split(" ");
-        fields = point
-            .intField("a", light[0])
-            .intField("b", light[1])
-            .intField("c", light[2])
-            .intField("d", light[3]);
-    } else {
-        const data = parseFloat(messageJson.data);
-        fields = point.floatField("data", data);
-    }
+    const data = parseFloat(messageJson.data);
+    fields = point.floatField("data", data);
     writeClient.writePoint(fields);
-    console.log(` ${fields}`);
+    console.log(`${fields}`);
+  }
 }
 
 module.exports = { dbAddData };
