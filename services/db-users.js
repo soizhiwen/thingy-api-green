9df02dac769a7e823eb675f21d3e9986beb681fc
soizhiwen <zhiwen39@gmail.com>
2023-11-18 21:40:10 +0100

    Add pg for users

--- services/db-users.js
+++ services/db-users.js
@@ -1,123 +1,202 @@
-
+const { pool } = require("../models/pg");
 
 async function dbListAdmins() {
-    console.log("Received List Admins request.");
-    const admins = {};
+  console.log("Received List Admins request.");
 
-    // Retrieve Users list from DB
-
-
-    let status = 501;
-    return {status, admins};
+  try {
+    const query = {
+      text: "SELECT * FROM users WHERE role=$1 ORDER BY id ASC;",
+      values: ["Admin"],
+    };
+    const { rows } = await pool.query(query);
+    return { status: 200, admins: rows };
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
-
 
 async function dbListUsers() {
-    console.log("Received List Users request.");
-    const users = {};
+  console.log("Received List Users request.");
 
-    // Retrieve Users list from DB
-    
-    
-    let status = 501;
-    return {status, users};
+  try {
+    const query = {
+      text: "SELECT * FROM users WHERE role=$1 ORDER BY id ASC;",
+      values: ["User"],
+    };
+    const { rows } = await pool.query(query);
+    return { status: 200, users: rows };
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
-
 
 async function dbGetAdminById(id) {
-    console.log("Received request for admin by ID: " + id);
-    const admin = {};
+  console.log(`Received request for admin by ID: ${id}`);
 
-    // Retrieve Admin by ID from DB
+  try {
+    const query = {
+      text: "SELECT * FROM users WHERE role=$1 AND id=$2;",
+      values: ["Admin", id],
+    };
+    const { rows } = await pool.query(query);
 
+    if (rows.length === 0) {
+      return 404;
+    }
 
-    let status = 501;
-    return {status, admin};
+    return { status: 200, admin: rows };
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
-
 
 async function dbGetUserById(id) {
-    console.log("Received request for User by ID: " + id);
-    const user = {};
+  console.log(`Received request for user by ID: ${id}`);
 
-    // Retrieve Admin by ID from DB
+  try {
+    const query = {
+      text: "SELECT * FROM users WHERE role=$1 AND id=$2;",
+      values: ["User", id],
+    };
+    const { rows } = await pool.query(query);
 
+    if (rows.length === 0) {
+      return 404;
+    }
 
-    let status = 501;
-    return {status, user};
+    return { status: 200, user: rows };
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
 
+async function dbRegisterAdmin(params) {
+  console.log(`Received Add Admin Request: ${JSON.stringify(params)}`);
 
-async function dbRegisterAdmin(adminParams) {
-    console.log("Received Add Admin Request with various data: " +
-        adminParams.name + " " + adminParams.email);
-    const adminId = "An Admin ID";
-    // Add Admin to DB
-
-    // if (ok) {return 201}
-    // else {return 500}
-    let status = 501;
-    return {status, adminId};
-
-}
-
-
-async function dbAddUser(userParams) {
-    console.log("Received Add User Request with various data: " +
-        userParams.name + " " + userParams.email);
-    const userId = "A User ID";
-    // Add User to DB
-
-    // if (ok) {return 201}
-    // else {return 500}
-    let status = 501;
-    return {status, userId};
-}
-
-
-async function dbUpdateAdmin(adminId, updateParams) {
-    console.log("Received Update Admin request with plant ID and data: " +
-        adminId + " " + updateParams.name + " " + updateParams.email);
-    // Update admin (adminID) with provided data
-    // if (ok) {return 210}
-    // else {return 500}
-
+  try {
+    const query = {
+      text: "INSERT INTO users(name, email, role) VALUES ($1, $2, $3) RETURNING id;",
+      values: [params.name, params.email, params.role],
+    };
+    const { rows } = await pool.query(query);
+    return { status: 201, adminId: rows[0].id };
+  } catch (err) {
+    console.log(err);
     return 501;
+  }
 }
 
+async function dbAddUser(params) {
+  console.log(`Received Add User Request: ${JSON.stringify(params)}`);
 
-async function dbUpdateUser(userId, updateParams) {
-    console.log("Received Update User request with plant ID and data: " +
-        userId + " " + updateParams.name + " " + updateParams.email);
-    // Update user (userID) with provided data
-    // if (ok) {return 210}
-    // else {return 500}
-
+  try {
+    const query = {
+      text: "INSERT INTO users(name, email, role) VALUES ($1, $2, $3) RETURNING id;",
+      values: [params.name, params.email, params.role],
+    };
+    const { rows } = await pool.query(query);
+    return { status: 201, userId: rows[0].id };
+  } catch (err) {
+    console.log(err);
     return 501;
+  }
 }
 
+async function dbUpdateAdmin(id, params) {
+  console.log(`Received Update Admin request: ${JSON.stringify(params)}`);
+  try {
+    const query = {
+      text: "UPDATE users SET name=$1, email=$2, role=$3 WHERE id=$4 RETURNING *;",
+      values: [params.name, params.email, params.role, id],
+    };
+    const { rows } = await pool.query(query);
 
-async function dbRemoveAdmin(adminId) {
-    console.log("Admin ID: " + adminId);
-    // Remove admin (adminId) from DB
+    if (rows.length === 0) {
+      return 404;
+    }
 
-    // if (ok) {return 210}
-    // else {return 500}
-
-    return 501;
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
 
+async function dbUpdateUser(id, params) {
+  console.log(`Received Update User request: ${JSON.stringify(params)}`);
+  try {
+    const query = {
+      text: "UPDATE users SET name=$1, email=$2, role=$3 WHERE id=$4 RETURNING *;",
+      values: [params.name, params.email, params.role, id],
+    };
+    const { rows } = await pool.query(query);
 
-async function dbRemoveUser(userId) {
-    console.log("User ID: " + userId);
-    // Remove user (userId) from DB
+    if (rows.length === 0) {
+      return 404;
+    }
 
-    // if (ok) {return 210}
-    // else {return 500}
-
-    return 501;
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
 
+async function dbRemoveAdmin(id) {
+  console.log(`Received Delete Admin request: ${id}`);
 
+  try {
+    const query = {
+      text: "DELETE FROM users WHERE id=$1 RETURNING *;",
+      values: [id],
+    };
+    const { rows } = await pool.query(query);
 
-module.exports = { dbListAdmins, dbListUsers, dbGetAdminById, dbGetUserById, dbRegisterAdmin, dbAddUser, dbUpdateAdmin, dbUpdateUser, dbRemoveAdmin, dbRemoveUser };
+    if (rows.length === 0) {
+      return 404;
+    }
+
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
+}
+
+async function dbRemoveUser(id) {
+  console.log(`Received Delete User request: ${id}`);
+
+  try {
+    const query = {
+      text: "DELETE FROM users WHERE id=$1 RETURNING *;",
+      values: [id],
+    };
+    const { rows } = await pool.query(query);
+
+    if (rows.length === 0) {
+      return 404;
+    }
+
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
+}
+
+module.exports = {
+  dbListAdmins,
+  dbListUsers,
+  dbGetAdminById,
+  dbGetUserById,
+  dbRegisterAdmin,
+  dbAddUser,
+  dbUpdateAdmin,
+  dbUpdateUser,
+  dbRemoveAdmin,
+  dbRemoveUser,
+};
