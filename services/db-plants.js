ec55211d5d9713e27f437b3fb75128a64acdbd3b
soizhiwen <zhiwen39@gmail.com>
2023-11-18 21:00:16 +0100

    Add pg for plants

--- services/db-plants.js
+++ services/db-plants.js
@@ -1,57 +1,89 @@
-
+const { pool } = require("../models/pg");
 
 async function dbListPlants() {
-    console.log("Received List Plants request.")
-    const plants = {};
+  console.log("Received List Plants request.");
 
-    // Retrieve Plants list from DB
-    
-    
-    let status = 501;
-    return {status, plants};
+  try {
+    const query = "SELECT * FROM plants ORDER BY id ASC;";
+    const { rows } = await pool.query(query);
+    return { status: 200, plants: rows };
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
 
+async function dbAddPlant(params) {
+  console.log(`Received Add Plant request: ${JSON.stringify(params)}`);
 
-async function dbAddPlant(plantParams) {
-    console.log("Received Add Plant Request with various data: " +
-        plantParams.name + " " + plantParams.temp + " " + plantParams.humidity + " " +
-        plantParams.co2 + " " + plantParams.airQual + " " + plantParams.growDuration);
-    const plandId = "A Plant ID";
-    // Add Plant to DB
-
-    // if (ok) {return 201}
-    // else {return 500}
+  try {
+    const query = {
+      text: "INSERT INTO plants(name, harvest_date, temperature, humidity, co2, airquality) VALUES ($1, $2, $3, $4, $5, $6);",
+      values: [
+        params.name,
+        params.harvest_date,
+        params.temperature,
+        params.humidity,
+        params.co2,
+        params.airQuality,
+      ],
+    };
+    await pool.query(query);
+    return 201;
+  } catch (err) {
+    console.log(err);
     return 501;
+  }
 }
 
+async function dbUpdatePlant(id, params) {
+  console.log(`Received Update Plant request: ${JSON.stringify(params)}`);
 
-async function dbUpdatePlant(plantId, updateParams) {
-    console.log("Received Update Plant request with plant ID and data: " + plantId + " " +
-        updateParams.name + " " + updateParams.temp + " " + updateParams.humidity + " " +
-        updateParams.co2 + " " + updateParams.airQual + " " + updateParams.growDuration);
-    // Update pland (plandID) with provided data
-    // if (ok) {return 210}
-    // else {return 500}
+  try {
+    const query = {
+      text: "UPDATE plants SET name=$1, harvest_date=$2, temperature=$3, humidity=$4, co2=$5, airquality=$6 WHERE id=$7 RETURNING *;",
+      values: [
+        params.name,
+        params.harvest_date,
+        params.temperature,
+        params.humidity,
+        params.co2,
+        params.airQuality,
+        id,
+      ],
+    };
+    const { rows } = await pool.query(query);
 
-    return 501;
+    if (rows.length === 0) {
+      return 404;
+    }
+
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
 
+async function dbRemovePlant(id) {
+  console.log(`Received Delete Plant request: ${id}`);
 
-async function dbRemovePlant(plantId) {
-    console.log("Plant ID: " + plantId);
-    // Remove plant (plantId) from DB
+  try {
+    const query = {
+      text: "DELETE FROM plants WHERE id=$1 RETURNING *;",
+      values: [id],
+    };
+    const { rows } = await pool.query(query);
 
-    // if (ok) {return 210}
-    // else {return 500}
+    if (rows.length === 0) {
+      return 404;
+    }
 
-    return 501;
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
 }
-
-
-
-
-
 
 module.exports = { dbListPlants, dbAddPlant, dbUpdatePlant, dbRemovePlant };
-
-
