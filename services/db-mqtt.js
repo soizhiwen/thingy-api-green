2cb812412f7210afa8975401b094a5c61e1b5fa8
GrischuG <GrischuG@protonmail.com>
2023-11-20 23:06:16 +0100

    changed add Data function
    thingy_monitor is now passed explicitly

--- services/db-mqtt.js
+++ services/db-mqtt.js
@@ -2,14 +2,14 @@
 const { writeClient } = require("../models/influx");
 const { Point } = require("@influxdata/influxdb-client");
 
-const thingyMonitor = process.env.THINGY_MONITOR;
+//const thingyMonitor = process.env.THINGY_MONITOR;
 const appIds = ["AIR_QUAL", "CO2_EQUIV", "TEMP", "HUMID"];
 
-async function dbAddData(message) {
+async function dbAddData(message, thingy_monitor) {
   const messageJson = JSON.parse(message.toString());
 
   if (appIds.includes(messageJson.appId)) {
-    writeClient.useDefaultTags({ thingy_id: thingyMonitor });
+    writeClient.useDefaultTags({ thingy_id: thingy_monitor });
 
     const point = new Point("mqtt_message").tag("app_id", messageJson.appId);
     const data = parseFloat(messageJson.data);
