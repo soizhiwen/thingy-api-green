3224b4a8bff011243087ae9a42036effdf078143
soizhiwen <zhiwen39@gmail.com>
2023-11-23 10:29:33 +0100

    Refactor users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,128 +1,69 @@
-const router = require('koa-router')();
+const router = require("koa-router")();
 
-const { dbListAdmins, dbListUsers, dbGetAdminById, dbGetUserById, dbRegisterAdmin, dbAddUser,
-        dbUpdateAdmin, dbUpdateUser, dbRemoveAdmin, dbRemoveUser}
-    = require("../services/db-users");
+const {
+  dbListUsers,
+  dbGetUserById,
+  dbCreateUser,
+  dbUpdateUser,
+  dbDeleteUser,
+} = require("../services/db-users");
 
-router.get('/users/admins/', listAdmins)
-    .get('/users/', listUsers)
-    .get('/users/admins/:id', getAdminById)
-    .get('/users/:id', getUserById)
-    .post('/users/admins/',registerAdmin)
-    .post('/users/', addUser)
-    .patch('/users/:id', updateUser)
-    .patch('/users/admins/:id', updateAdmin)
-    .del('/users/:id', removeUser)
-    .del('/users/admin/:id', removeAdmin);
-
-
-async function listAdmins(ctx) {
-    console.log("GET request to list all Admins received!");
-    const {status, admins} = await dbListAdmins();
-
-    // Return all admins
-    ctx.body = admins;
-    ctx.status = status;
-}
-
+router
+  .get("/users/", listUsers)
+  .get("/users/:id", getUserById)
+  .post("/users/", createUser)
+  .patch("/users/:id", updateUser)
+  .del("/users/:id", deleteUser);
 
 async function listUsers(ctx) {
-    console.log("GET request to list all users received!");
-    const {status, users} = await dbListUsers();
+  console.log("GET request to list all users received!");
+  const { status, body } = await dbListUsers();
 
-    // Return all users
-    ctx.body = users;
-    ctx.status = status;
+  // Return all users
+  ctx.body = body;
+  ctx.status = status;
 }
-
-
-async function getAdminById(ctx) {
-    console.log("GET request for one Admin by ID received!");
-    const adminId = ctx.params.id;
-    const {status, admin} = await dbGetAdminById(adminId);
-
-    // Return admin
-    ctx.body = admin;
-    ctx.status = status;
-}
-
 
 async function getUserById(ctx) {
-    console.log("GET request for one User by ID received!");
-    const userId = ctx.params.id;
-    const {status, user} = await dbGetUserById(userId);
+  console.log("GET request for one User by ID received!");
+  const id = ctx.params.id;
+  const { status, body } = await dbGetUserById(id);
 
-    // Return admin
-    ctx.body = user;
-    ctx.status = status;
+  // Return user
+  ctx.body = body;
+  ctx.status = status;
 }
 
+async function createUser(ctx) {
+  console.log("POST request to add a user received!");
+  const params = ctx.request.body;
 
-async function registerAdmin(ctx) {
-    console.log("POST request to register admin!");
-    const adminParams = ctx.request.body;
-
-    // Call DB Function
-    const {status, adminId} = await dbRegisterAdmin(adminParams);
-
-    ctx.body = adminId;
-    ctx.status = status;
+  // Call DB Function
+  const { status, body } = await dbCreateUser(params);
+  ctx.body = body;
+  ctx.status = status;
 }
-
-
-async function addUser(ctx) {
-    console.log("POST request to add a user received!");
-    const userParams = ctx.request.body;
-
-    // Call DB Function
-    const {status, userId} = await dbAddUser(userParams);
-    ctx.body = userId;
-    ctx.status = status;
-}
-
-
-async function updateAdmin(ctx) {
-    console.log("PATCH request to change an admin's value received!");
-
-    const adminId = ctx.params.id;
-    const updateParams = ctx.request.body;
-
-    // Update in DB
-    const status = await dbUpdateAdmin(adminId, updateParams);
-    ctx.status = status;
-}
-
 
 async function updateUser(ctx) {
-    console.log("PATCH request to change a user's value received!");
+  console.log("PATCH request to change a user's value received!");
 
-    const userId = ctx.params.id;
-    const updateParams = ctx.request.body;
+  const id = ctx.params.id;
+  const params = ctx.request.body;
 
-    // Update in DB
-    const status = await dbUpdateUser(userId, updateParams);
-    ctx.status = status; 
+  // Update in DB
+  const { status, body } = await dbUpdateUser(id, params);
+  ctx.body = body;
+  ctx.status = status;
 }
 
+async function deleteUser(ctx) {
+  console.log("DELETE request received!");
+  const id = ctx.params.id;
 
-async function removeAdmin(ctx) {
-    console.log("DELETE request received!");
-    const adminId = ctx.params.id;
-
-    // Remove admin from DB
-    const status = await dbRemoveAdmin(adminId);
-    ctx.status = status;
+  // Remove user from DB
+  const { status, body } = await dbDeleteUser(id);
+  ctx.body = body;
+  ctx.status = status;
 }
 
-
-async function removeUser(ctx) {
-    console.log("DELETE request received!");
-    const userId = ctx.params.id;
-
-    // Remove user from DB
-    const status = await dbRemoveUser(userId);
-    ctx.status = status;
-}
-
-
-module.exports=router;
+module.exports = router;
--- services/db-users.js
+++ services/db-users.js
@@ -1,52 +1,12 @@
 const { pool } = require("../models/pg");
 
-async function dbListAdmins() {
-  console.log("Received List Admins request.");
-
-  try {
-    const query = {
-      text: "SELECT * FROM users WHERE role=$1 ORDER BY id ASC;",
-      values: ["Admin"],
-    };
-    const { rows } = await pool.query(query);
-    return { status: 200, admins: rows };
-  } catch (err) {
-    console.log(err);
-    return 500;
-  }
-}
-
 async function dbListUsers() {
   console.log("Received List Users request.");
 
   try {
-    const query = {
-      text: "SELECT * FROM users WHERE role=$1 ORDER BY id ASC;",
-      values: ["User"],
-    };
+    const query = "SELECT * FROM users ORDER BY id ASC;";
     const { rows } = await pool.query(query);
-    return { status: 200, users: rows };
-  } catch (err) {
-    console.log(err);
-    return 500;
-  }
-}
-
-async function dbGetAdminById(id) {
-  console.log(`Received request for admin by ID: ${id}`);
-
-  try {
-    const query = {
-      text: "SELECT * FROM users WHERE role=$1 AND id=$2;",
-      values: ["Admin", id],
-    };
-    const { rows } = await pool.query(query);
-
-    if (rows.length === 0) {
-      return 404;
-    }
-
-    return { status: 200, admin: rows };
+    return { status: 200, body: rows };
   } catch (err) {
     console.log(err);
     return 500;
@@ -58,8 +18,8 @@ async function dbGetUserById(id) {
 
   try {
     const query = {
-      text: "SELECT * FROM users WHERE role=$1 AND id=$2;",
-      values: ["User", id],
+      text: "SELECT * FROM users WHERE id=$1;",
+      values: [id],
     };
     const { rows } = await pool.query(query);
 
@@ -67,67 +27,32 @@ async function dbGetUserById(id) {
       return 404;
     }
 
-    return { status: 200, user: rows };
+    return { status: 200, body: rows };
   } catch (err) {
     console.log(err);
     return 500;
   }
 }
 
-async function dbRegisterAdmin(params) {
-  console.log(`Received Add Admin Request: ${JSON.stringify(params)}`);
-
-  try {
-    const query = {
-      text: "INSERT INTO users(name, email, role) VALUES ($1, $2, $3) RETURNING id;",
-      values: [params.name, params.email, params.role],
-    };
-    const { rows } = await pool.query(query);
-    return { status: 201, adminId: rows[0].id };
-  } catch (err) {
-    console.log(err);
-    return 501;
-  }
-}
-
-async function dbAddUser(params) {
+async function dbCreateUser(params) {
   console.log(`Received Add User Request: ${JSON.stringify(params)}`);
 
   try {
     const query = {
-      text: "INSERT INTO users(name, email, role) VALUES ($1, $2, $3) RETURNING id;",
+      text: "INSERT INTO users(name, email, role) VALUES ($1, $2, $3) RETURNING *;",
       values: [params.name, params.email, params.role],
     };
     const { rows } = await pool.query(query);
-    return { status: 201, userId: rows[0].id };
+    return { status: 201, body: rows };
   } catch (err) {
     console.log(err);
     return 501;
-  }
-}
-
-async function dbUpdateAdmin(id, params) {
-  console.log(`Received Update Admin request: ${JSON.stringify(params)}`);
-  try {
-    const query = {
-      text: "UPDATE users SET name=$1, email=$2, role=$3 WHERE id=$4 RETURNING *;",
-      values: [params.name, params.email, params.role, id],
-    };
-    const { rows } = await pool.query(query);
-
-    if (rows.length === 0) {
-      return 404;
-    }
-
-    return 200;
-  } catch (err) {
-    console.log(err);
-    return 500;
   }
 }
 
 async function dbUpdateUser(id, params) {
   console.log(`Received Update User request: ${JSON.stringify(params)}`);
+
   try {
     const query = {
       text: "UPDATE users SET name=$1, email=$2, role=$3 WHERE id=$4 RETURNING *;",
@@ -139,40 +64,19 @@ async function dbUpdateUser(id, params) {
       return 404;
     }
 
-    return 200;
+    return { status: 200, body: rows };
   } catch (err) {
     console.log(err);
     return 500;
   }
 }
 
-async function dbRemoveAdmin(id) {
-  console.log(`Received Delete Admin request: ${id}`);
-
-  try {
-    const query = {
-      text: "DELETE FROM users WHERE id=$1 RETURNING *;",
-      values: [id],
-    };
-    const { rows } = await pool.query(query);
-
-    if (rows.length === 0) {
-      return 404;
-    }
-
-    return 200;
-  } catch (err) {
-    console.log(err);
-    return 500;
-  }
-}
-
-async function dbRemoveUser(id) {
+async function dbDeleteUser(id) {
   console.log(`Received Delete User request: ${id}`);
 
   try {
     const query = {
-      text: "DELETE FROM users WHERE id=$1 RETURNING *;",
+      text: "DELETE FROM users WHERE id=$1 RETURNING id;",
       values: [id],
     };
     const { rows } = await pool.query(query);
@@ -181,7 +85,7 @@ async function dbRemoveUser(id) {
       return 404;
     }
 
-    return 200;
+    return { status: 200, body: rows[0].id };
   } catch (err) {
     console.log(err);
     return 500;
@@ -189,14 +93,9 @@ async function dbRemoveUser(id) {
 }
 
 module.exports = {
-  dbListAdmins,
   dbListUsers,
-  dbGetAdminById,
   dbGetUserById,
-  dbRegisterAdmin,
-  dbAddUser,
-  dbUpdateAdmin,
+  dbCreateUser,
   dbUpdateUser,
-  dbRemoveAdmin,
-  dbRemoveUser,
+  dbDeleteUser,
 };
