e88e58f49f1ae83eeedaeafa883255eaa6e73493
GrischuG <GrischuG@protonmail.com>
2023-11-29 13:03:55 +0100

    minor

--- services/notification-service.js
+++ services/notification-service.js
@@ -1,6 +1,11 @@
 const { enableBuzzer, disableBuzzer, setLEDRed, setLEDBlue } = require("./mqtt");
 
-
+/**
+ * WHAT WE NEED:
+ *
+ * - Logic: Depending on the thingy/sensor data, send notification to client and activate
+ *      thingy/notification with respective behaviour (buzzer, LED)
+ */
 
 
 
