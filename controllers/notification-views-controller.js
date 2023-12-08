376fcccf35071c0f48c78f49f92b9faf01d2341d
SachinduHimash <himash1997@gmail.com>
2023-12-08 14:54:27 +0100

    Notification View Backend

--- controllers/notification-views-controller.js
+++ controllers/notification-views-controller.js
@@ -5,14 +5,15 @@ const {
   dbCreateNotificationViews,
 } = require("../services/db-notification-views");
 
-const { verifyToken } = require("../services/auth-JWT");
+const { verifyToken, getUserId } = require("../services/auth-JWT");
 
 router
-  .get("/notification-views/:id", verifyToken, getNotificationViewsById)
+  .get("/notification-views/", verifyToken, getNotificationViewsById)
   .post("/notification-views/", verifyToken, createNotificationViews);
 
 async function getNotificationViewsById(ctx) {
   const id = ctx.params.id;
+  getUserId(ctx).then((userId)=>  console.log('Get the userId from here',userId));
   const { status, body } = await dbGetNotificationViewsById(id);
   ctx.body = body;
   ctx.status = status;
