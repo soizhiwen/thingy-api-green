fcc12bf2eb8fc1268e720d0a69740a7aa6298c37
soizhiwen <zhiwen39@gmail.com>
2023-12-09 10:26:43 +0100

    Minor update

--- controllers/notification-views-controller.js
+++ controllers/notification-views-controller.js
@@ -9,7 +9,7 @@ const { verifyToken, getUserId } = require("../services/auth-JWT");
 
 router
   .get("/notification-views/", verifyToken, getNotificationViewsByUserId)
-  .patch("/notification-views/:id", verifyToken, updateNotificationViews);
+  .patch("/notification-views/", verifyToken, updateNotificationViews);
 
 async function getNotificationViewsByUserId(ctx) {
   const id = await getUserId(ctx);
@@ -19,7 +19,7 @@ async function getNotificationViewsByUserId(ctx) {
 }
 
 async function updateNotificationViews(ctx) {
-  const id = ctx.params.id;
+  const id = await getUserId(ctx);
   const { status, body } = await dbUpdateNotificationViews(id);
   ctx.body = body;
   ctx.status = status;
